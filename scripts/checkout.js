bff065859ab41f209bf5c6406b52ef90e0b4d979
Xeyn <Edgarrodilorosa@gmail.com>
2025-01-20 15:37:19 +0800

    delete products from HTML when clicking delete link

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,10 +16,11 @@ cart.forEach((cartItem) => {
             matchingProduct = product;
         }
     });
-    
+
 
     cartSummaryHTML += `
-     <div class="cart-item-container">
+     <div class="cart-item-container 
+      js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -105,6 +106,10 @@ document.querySelectorAll('.js-delete-link').forEach((deleteLink) => {
   deleteLink.addEventListener('click', () => {  
     const {productId} = deleteLink.dataset;
     removeFromCart(productId);
+
+   const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+   container.remove();
 
   });
 });
