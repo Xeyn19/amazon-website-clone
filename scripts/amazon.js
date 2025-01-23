fc8ce7095cf21710431253d08801cdfd2c5162de
Xeyn <Edgarrodilorosa@gmail.com>
2025-01-23 15:25:26 +0800

    Save data and generate  HTML for delivery options

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart} from "../data/cart.js";
+import {cart, addToCart, calculateCartQuantity} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -62,13 +62,9 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity(){
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-   cartQuantity += cartItem.quantity  
-  });
- 
+ const cartQuantity = calculateCartQuantity();
  document.querySelector('.js-total-quantity').innerHTML = cartQuantity;
+
  
 }
 
@@ -100,4 +96,4 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 });
 
-window.onload = updateCartQuantity;
+window.onload = updateCartQuantity();
